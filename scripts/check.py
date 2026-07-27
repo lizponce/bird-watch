69985cbc793c9b809dcd461057e620c0ec2ed9fa
Liz Ponce <liz@lzrmarketing.com>
2026-07-27 09:41:09 -0700

    Updated encoding

--- scripts/check.py
+++ scripts/check.py
@@ -56,7 +56,7 @@ def check_site(site):
                 "User-Agent": UA,
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                 "Accept-Language": "en-US,en;q=0.9",
-                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Encoding": "gzip, deflate",
             },
             allow_redirects=True,
         )
